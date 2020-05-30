34d842d04e42fe7a6e3291eafde3c070c295b156
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-05-30 16:34:52 +0900

    Add WalletFeature.from_wif method

--- lib/tapyrus/contract/wallet_feature.rb
+++ lib/tapyrus/contract/wallet_feature.rb
@@ -1,6 +1,10 @@
 module Tapyrus
   module Contract
     module WalletFeature
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+ 
       def to_p2pkh
         Tapyrus::Script.to_p2pkh(key.hash160).to_payload
       end
@@ -11,6 +15,16 @@ module Tapyrus
 
       def key
         @key ||= Tapyrus::Key.generate
+      end
+
+      def key=(key)
+        @key = key
+      end
+
+      module ClassMethods
+        def from_wif(wif)
+          new.tap { |wallet| wallet.key = Tapyrus::Key.from_wif(wif) }
+        end
       end
     end
   end
