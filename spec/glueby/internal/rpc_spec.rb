6daae954e1ca637d3999c3f7b0b88d0fca1ddb4d
Kohei Taniguchi <kohei@chaintope.com>
2020-07-27 11:51:33 +0900

    Rename switch_wallet to perform_as
    And remove a use-case that is called on no-block.

--- spec/glueby/internal/rpc_spec.rb
+++ spec/glueby/internal/rpc_spec.rb
@@ -1,34 +1,31 @@
 RSpec.describe 'Glueby::Internal::RPC' do
-  describe 'switch_wallet' do
-    subject { Glueby::Internal::RPC.switch_wallet(wallet_name) }
+  describe 'perform_as' do
+    subject { Glueby::Internal::RPC.perform_as(wallet_name) }
     let(:wallet_name) { 'wallet' }
 
     before do
       Glueby::Internal::RPC.configure(config)
     end
 
-    let(:config) { {schema: 'http', host: '127.0.0.1', port: 12381, user: 'user', password: 'pass'} }
-
-    it 'should change the config[:wallet] value inner RPC client' do
-      expect(subject).to eq wallet_name
-      expect(Glueby::Internal::RPC.client.config[:wallet]).to eq wallet_name
+    after do
+      Glueby::Internal::RPC.instance_variable_set(:@rpc, nil)
     end
 
-    context 'It is received a block argument' do
-      it 'should switch wallet only while processing the block' do
-        rt = Glueby::Internal::RPC.switch_wallet(wallet_name) do |client|
-          # It should update config in the block
-          expect(client.config[:wallet]).to eq wallet_name
+    let(:config) { { schema: 'http', host: '127.0.0.1', port: 12381, user: 'user', password: 'pass' } }
 
-          'Return value of the block'
-        end
+    it 'should switch wallet only while processing the block' do
+      rt = Glueby::Internal::RPC.perform_as(wallet_name) do |client|
+        # It should update config in the block
+        expect(client.config[:wallet]).to eq wallet_name
 
-        # It should revert the config.
-        expect(Glueby::Internal::RPC.client.config[:wallet]).to be_nil
-
-        # It should returns the value the block returns.
-        expect(rt).to eq 'Return value of the block'
+        'Return value of the block'
       end
+
+      # It should revert the config.
+      expect(Glueby::Internal::RPC.client.config[:wallet]).to be_nil
+
+      # It should returns the value the block returns.
+      expect(rt).to eq 'Return value of the block'
     end
   end
 end
