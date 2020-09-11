39e7b01345ca557c3153fd886b8751a65cc9b9f8
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-09-11 17:13:45 +0900

    Add TokenTxBuilder#dummy_tx method to calculate tx fee

--- lib/glueby/contract/token_tx_builder.rb
+++ lib/glueby/contract/token_tx_builder.rb
@@ -10,7 +10,7 @@ module Glueby
       # Create new public key, and new transaction that sends TPC to it
       def create_funding_tx(wallet:, amount:, script: nil, fee_provider: FixedFeeProvider.new)
         tx = Tapyrus::Tx.new
-        fee = fee_provider.fee(tx)
+        fee = fee_provider.fee(dummy_tx(tx))
 
         utxos = wallet.internal_wallet.list_unspent
         sum, outputs = collect_uncolored_outputs(utxos, fee + amount)
@@ -25,7 +25,6 @@ module Glueby
 
       def create_issue_tx_for_reissuable_token(funding_tx:, issuer:, amount:, fee_provider: FixedFeeProvider.new)
         tx = Tapyrus::Tx.new
-        fee = fee_provider.fee(tx)
 
         out_point = Tapyrus::OutPoint.from_txid(funding_tx.txid, 0)
         tx.inputs << Tapyrus::TxIn.new(out_point: out_point)
@@ -36,6 +35,7 @@ module Glueby
         receiver_colored_script = receiver_script.add_color(color_id)
         tx.outputs << Tapyrus::TxOut.new(value: amount, script_pubkey: receiver_colored_script)
 
+        fee = fee_provider.fee(dummy_tx(tx))
         fill_change_tpc(tx, issuer, output.value - fee)
         prev_txs = [{
           txid: funding_tx.txid,
@@ -56,8 +56,8 @@ module Glueby
 
       def create_issue_tx_from_out_point(token_type:, issuer:, amount:, fee_provider: FixedFeeProvider.new)
         tx = Tapyrus::Tx.new
-        fee = fee_provider.fee(tx)
 
+        fee = fee_provider.fee(dummy_issue_tx_from_out_point)
         utxos = issuer.internal_wallet.list_unspent
         sum, outputs = collect_uncolored_outputs(utxos, fee)
         fill_input(tx, outputs)
@@ -82,7 +82,6 @@ module Glueby
 
       def create_reissue_tx(funding_tx:, issuer:, amount:, color_id:, fee_provider: FixedFeeProvider.new)
         tx = Tapyrus::Tx.new
-        fee = fee_provider.fee(tx)
 
         out_point = Tapyrus::OutPoint.from_txid(funding_tx.txid, 0)
         tx.inputs << Tapyrus::TxIn.new(out_point: out_point)
@@ -92,6 +91,7 @@ module Glueby
         receiver_colored_script = receiver_script.add_color(color_id)
         tx.outputs << Tapyrus::TxOut.new(value: amount, script_pubkey: receiver_colored_script)
 
+        fee = fee_provider.fee(dummy_tx(tx))
         fill_change_tpc(tx, issuer, output.value - fee)
         prev_txs = [{
           txid: funding_tx.txid,
@@ -104,12 +104,8 @@ module Glueby
 
       def create_transfer_tx(color_id:, sender:, receiver:, amount:, fee_provider: FixedFeeProvider.new)
         tx = Tapyrus::Tx.new
-        fee = fee_provider.fee(tx)
 
         utxos = sender.internal_wallet.list_unspent
-        sum_tpc, outputs = collect_uncolored_outputs(utxos, fee)
-        fill_input(tx, outputs)
-
         sum_token, outputs = collect_colored_outputs(utxos, color_id, amount)
         fill_input(tx, outputs)
 
@@ -118,23 +114,29 @@ module Glueby
         tx.outputs << Tapyrus::TxOut.new(value: amount, script_pubkey: receiver_colored_script)
 
         fill_change_token(tx, sender, sum_token - amount, color_id)
+
+        fee = fee_provider.fee(dummy_tx(tx))
+        sum_tpc, outputs = collect_uncolored_outputs(utxos, fee)
+        fill_input(tx, outputs)
+
         fill_change_tpc(tx, sender, sum_tpc - fee)
         sender.internal_wallet.sign_tx(tx)
       end
 
       def create_burn_tx(color_id:, sender:, amount: 0, fee_provider: FixedFeeProvider.new)
         tx = Tapyrus::Tx.new
-        fee = fee_provider.fee(tx)
 
         utxos = sender.internal_wallet.list_unspent
-        dust = 600 # in case that the wallet has output which has just fee amount.
-        sum_tpc, outputs = collect_uncolored_outputs(utxos, fee + dust)
-        fill_input(tx, outputs)
-
         sum_token, outputs = collect_colored_outputs(utxos, color_id, amount)
         fill_input(tx, outputs)
 
         fill_change_token(tx, sender, sum_token - amount, color_id) if amount.positive?
+
+        fee = fee_provider.fee(dummy_tx(tx))
+
+        dust = 600 # in case that the wallet has output which has just fee amount.
+        sum_tpc, outputs = collect_uncolored_outputs(utxos, fee + dust)
+        fill_input(tx, outputs)
 
         fill_change_tpc(tx, sender, sum_tpc - fee)
         sender.internal_wallet.sign_tx(tx)
@@ -193,6 +195,33 @@ module Glueby
         raise Glueby::Contract::Errors::InsufficientTokens if amount.positive?
 
         results
+      end
+
+      # Add dummy inputs and outputs to tx
+      def dummy_tx(tx)
+        dummy = Tapyrus::Tx.parse_from_payload(tx.to_payload)
+
+        # dummy input for tpc
+        out_point = Tapyrus::OutPoint.new('00' * 32, 0)
+        dummy.inputs << Tapyrus::TxIn.new(out_point: out_point)
+
+        # Add script_sig to all intpus
+        dummy.inputs.each do |input|
+          input.script_sig = Tapyrus::Script.parse_from_payload('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'.htb)
+        end
+
+        # dummy output to return change
+        change_script = Tapyrus::Script.to_p2pkh('0000000000000000000000000000000000000000')
+        dummy.outputs << Tapyrus::TxOut.new(value: 0, script_pubkey: change_script)
+        dummy
+      end
+
+      # Add dummy inputs and outputs to tx for issue non-reissuable transaction and nft transaction
+      def dummy_issue_tx_from_out_point
+        tx = Tapyrus::Tx.new
+        receiver_colored_script = Tapyrus::Script.parse_from_payload('21c20000000000000000000000000000000000000000000000000000000000000000bc76a914000000000000000000000000000000000000000088ac'.htb)
+        tx.outputs << Tapyrus::TxOut.new(value: 0, script_pubkey: receiver_colored_script)
+        dummy_tx(tx)
       end
     end
   end
