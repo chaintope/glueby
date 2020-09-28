ba12f18acadb1bb60c565310c69733c2d6489471
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-09-28 18:47:26 +0900

    Use Tapyrus::Script#color_id

--- lib/glueby/internal/wallet/active_record/utxo.rb
+++ lib/glueby/internal/wallet/active_record/utxo.rb
@@ -11,9 +11,7 @@ module Glueby
 
           def color_id
             script = Tapyrus::Script.parse_from_payload(script_pubkey.htb)
-            if script.cp2pkh? || script.cp2sh?
-              Tapyrus::Color::ColorIdentifier.parse_from_payload(script.chunks[0].pushed_data).to_hex
-            end
+            script.color_id&.to_hex
           end
         end
       end
