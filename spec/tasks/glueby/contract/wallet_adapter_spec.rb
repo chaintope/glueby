c468e324e2e2d3cd3340913c760e7c7447bb2666
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-09-30 00:54:06 +0900

    Update spec

--- spec/tasks/glueby/contract/wallet_adapter_spec.rb
+++ spec/tasks/glueby/contract/wallet_adapter_spec.rb
@@ -40,14 +40,22 @@ RSpec.describe 'Glueby::Contract::Task::WalletAdapter' do
 
   before(:each) do
     allow(Glueby::Internal::RPC).to receive(:client).and_return(rpc)
-    allow(rpc).to receive(:getblock).with('5498724b2d536c99b5a0533ebc2a5f00d59309cfe9f05e9a72a221343c240c5e', 0).and_return(response_getblock)
-    allow(rpc).to receive(:getrawtransaction).with('3c345488060a07bae910fe0879e0c72927cb0283f6757d92cf0ac5609c409b17').and_return(response_getrawtransaction)
-
+    allow(rpc).to receive(:getblock).with('022890167018b090211fb8ef26970c26a0cac6d29e5352f506dc31bbb84f3ce7', 0).and_return(response_getblock)
+    allow(rpc).to receive(:getrawtransaction).with('2acb0d1015c382d63d4d8404b3219fc37c2c5c49aa6d6994f654758fb0179071').and_return(response_getrawtransaction1)
+    allow(rpc).to receive(:getrawtransaction).with('b4d0dbafa6777d8a902cf4359bdf1bdca3dbaca9ad450f284530cf039f49a23b').and_return(response_getrawtransaction2)
     setup_database
 
     wallet = Glueby::Internal::Wallet::AR::Wallet.create(wallet_id: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF')
-    private_key='206f3acb5b7ac66dacf87910bb0b04bed78284b9b50c0d061705a44447a947ff'
     Glueby::Internal::Wallet::AR::Key.create(private_key: private_key, purpose: :change, wallet: wallet)
+
+    # utxo for input
+    Glueby::Internal::Wallet::AR::Utxo.create(
+      txid: 'd176b97d76488de0a85609c359eba1ceb357e739c334aa93ed16eff1fd86c06e',
+      index: 0,
+      script_pubkey: '76a9143f90406e69facde1c8b08ddd9cf3d41f69ff2c3b88ac',
+      value: 5_000_000_000,
+      status: :finalized
+    )
   end
 
   after do
@@ -66,45 +74,79 @@ RSpec.describe 'Glueby::Contract::Task::WalletAdapter' do
   end
 
   let(:config) { { adapter: 'sqlite3', database: 'test' } }
+  let(:private_key) { 'ab2e2ba4c0605a3bd4f7734807e3346bae01290ebbaa0289c5c36912f7343650' }
   let(:rpc) { double('mock') }
+
+  # This block has 2 transaction.
+  # The first is coinbase to address 16o6NsMF5BmN35smQxjTjHjyecR2fRUSVF
+  # The second is transfer TPC transaction 16o6NsMF5BmN35smQxjTjHjyecR2fRUSVF -> 16o6NsMF5BmN35smQxjTjHjyecR2fRUSVF
   let(:response_getblock) do
-    '01000000f5e01c832b49a073188a90a283e9f941483dd3b362ada3c69100a823' \
-    '42df74027c9cfe8681330cea81115f6e008d2c1fd20a2dd47c942b11129ce3e2' \
-    'b51656d2179b409c60c50acf927d75f68302cb2729c7e07908fe10e9ba070a06' \
-    '8854343cca4c735f0040a34a233ca3b90837c81992df91a562ebf5fe00a31fe7' \
-    'e4c39475b6ea9431cc46efabad5a7037e58992ceddd6dc6d483af39c40f5af64' \
-    '4adeff10a5322c45f11201010000000100000000000000000000000000000000' \
-    '000000000000000000000000000000000d000000035d0101ffffffff0100f205' \
-    '2a010000001976a91457dd450aed53d4e35d3555a24ae7dbf3e08a78ec88ac00' \
-    '000000'
+    '01000000953fec29387d5236e10fc3bd29dedafd0873551ec4cdfd0f9b07f020' \
+    '34fc45514726f04b560e689db3a2dfc1d714720704e2c1b67909541ae4ae02a6' \
+    'b4a0fa68e8ce7a8de5c1831a6543a1235a4a646fe9a5c0868623cc6d78249b62' \
+    'd39979d69737745f00401d01200783805741e17c577f59fe14213872592eeab7' \
+    '4a680fca45b9530ed8facb53666a6b481109532a1b8a2774776b2ab373ff87cf' \
+    'c2538cd39cf7d984515602010000000100000000000000000000000000000000' \
+    '000000000000000000000000000000001000000003600101ffffffff01101906' \
+    '2a010000001976a9143f90406e69facde1c8b08ddd9cf3d41f69ff2c3b88ac00' \
+    '00000001000000016ec086fdf1ef16ed93aa34c339e757b3cea1eb59c30956a8' \
+    'e08d48767db976d1000000006a47304402207a484912d6878f694b7a3f8fafc3' \
+    '271c3d961fd8c0bc14f1321cbdb153400a1a0220440da5c48982a072aa37f281' \
+    '5959cac8b7a3d650dc47d497bb75d5ad124ae39601210261f487323a75d17cb8' \
+    '6e9d745b8581afb8e98cb2b6151184bd12fd81c48fc167ffffffff01f0ca052a' \
+    '010000001976a9143f90406e69facde1c8b08ddd9cf3d41f69ff2c3b88ac0000' \
+    '0000' \
   end
-  let(:response_getrawtransaction) do
+
+  # coinbase
+  let(:response_getrawtransaction1) do
     '0100000001000000000000000000000000000000000000000000000000000000' \
-    '00000000000d000000035d0101ffffffff0100f2052a010000001976a91457dd' \
-    '450aed53d4e35d3555a24ae7dbf3e08a78ec88ac00000000'
+    '00000000001000000003600101ffffffff011019062a010000001976a9143f90' \
+    '406e69facde1c8b08ddd9cf3d41f69ff2c3b88ac00000000'
+  end
+
+  # transfer TPC
+  let(:response_getrawtransaction2) do
+    '01000000016ec086fdf1ef16ed93aa34c339e757b3cea1eb59c30956a8e08d48' \
+    '767db976d1000000006a47304402207a484912d6878f694b7a3f8fafc3271c3d' \
+    '961fd8c0bc14f1321cbdb153400a1a0220440da5c48982a072aa37f2815959ca' \
+    'c8b7a3d650dc47d497bb75d5ad124ae39601210261f487323a75d17cb86e9d74' \
+    '5b8581afb8e98cb2b6151184bd12fd81c48fc167ffffffff01f0ca052a010000' \
+    '001976a9143f90406e69facde1c8b08ddd9cf3d41f69ff2c3b88ac00000000'
   end
 
   describe '#import_block' do
-    subject { @rake['glueby:contract:wallet_adapter:import_block'].invoke('5498724b2d536c99b5a0533ebc2a5f00d59309cfe9f05e9a72a221343c240c5e') }
+    subject { @rake['glueby:contract:wallet_adapter:import_block'].invoke('022890167018b090211fb8ef26970c26a0cac6d29e5352f506dc31bbb84f3ce7') }
 
     after { @rake['glueby:contract:wallet_adapter:import_block'].reenable }
 
     it do
-      expect(rpc).to receive(:getrawtransaction).once
+      expect(rpc).to receive(:getblock).once
+      expect(rpc).to receive(:getrawtransaction).twice
       subject
+      expect(Glueby::Internal::Wallet::AR::Utxo::find_by(txid: 'd176b97d76488de0a85609c359eba1ceb357e739c334aa93ed16eff1fd86c06e', index: 0)).to be_nil
+      expect(Glueby::Internal::Wallet::AR::Utxo::find_by(txid: 'b4d0dbafa6777d8a902cf4359bdf1bdca3dbaca9ad450f284530cf039f49a23b', index: 0)).not_to be_nil
     end
-    it { expect { subject }.to change { Glueby::Internal::Wallet::AR::Utxo.count }.from(0).to(1) }
+    it { expect { subject }.to change { Glueby::Internal::Wallet::AR::Utxo.count }.from(1).to(2) }
   end
 
   describe '#import_tx' do
-    subject { @rake['glueby:contract:wallet_adapter:import_tx'].invoke('3c345488060a07bae910fe0879e0c72927cb0283f6757d92cf0ac5609c409b17') }
+    subject { @rake['glueby:contract:wallet_adapter:import_tx'].invoke('b4d0dbafa6777d8a902cf4359bdf1bdca3dbaca9ad450f284530cf039f49a23b') }
 
     after { @rake['glueby:contract:wallet_adapter:import_tx'].reenable }
 
     it do
       expect(rpc).to receive(:getrawtransaction).once
       subject
+      expect(Glueby::Internal::Wallet::AR::Utxo::find_by(txid: 'd176b97d76488de0a85609c359eba1ceb357e739c334aa93ed16eff1fd86c06e', index: 0)).to be_nil
+      expect(Glueby::Internal::Wallet::AR::Utxo::find_by(txid: 'b4d0dbafa6777d8a902cf4359bdf1bdca3dbaca9ad450f284530cf039f49a23b', index: 0)).not_to be_nil
     end
-    it { expect { subject }.to change { Glueby::Internal::Wallet::AR::Utxo.count }.from(0).to(1) }
+    it { expect { subject }.not_to change { Glueby::Internal::Wallet::AR::Utxo.count } }
+
+    context 'if tx is not associated with glueby wallet' do
+      let(:private_key) { '22f774bbcf6a39b3dbeb47761b4d83b5cb0c6cf558db7c400ddd2bbe19cc3e79' }
+
+      it { expect { subject }.to change { Glueby::Internal::Wallet::AR::Utxo.count }.from(1).to(0) }
+    end
   end
 end
