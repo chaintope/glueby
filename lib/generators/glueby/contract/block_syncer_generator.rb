37cea6f6925019629d096be0fcdcac2fc4beefec
okashiwa <tkm05oshikawa12@gmail.com>
2021-03-23 13:47:02 +0900

    fix migration bug on BlockSyncer

--- lib/generators/glueby/contract/block_syncer_generator.rb
+++ lib/generators/glueby/contract/block_syncer_generator.rb
@@ -1,21 +1,25 @@
 module Glueby
   module Contract
-    module BlockSyncerGenerator < Rails::Generators::Base
+    class BlockSyncerGenerator < Rails::Generators::Base
       include ::Rails::Generators::Migration
       include Glueby::Generator::MigrateGenerator
       extend Glueby::Generator::MigrateGenerator::ClassMethod
 
       source_root File.expand_path('templates', __dir__)
 
-      if self.class.migration_exists?(migration_dir, "create_system_information")
-        ::Kernel.warn "Migration already exists: create_system_information"
-      else
-        migration_template(
-          "system_information_table.rb.erb",
-          "db/migrate/create_system_information.rb",
-          migration_version: migration_version,
-          table_options: table_options,
-        )
+      def create_migration_file
+        migration_dir = File.expand_path("db/migrate")
+
+        if self.class.migration_exists?(migration_dir, "create_system_information")
+          ::Kernel.warn "Migration already exists: create_system_information"
+        else
+          migration_template(
+            "system_information_table.rb.erb",
+            "db/migrate/create_system_information.rb",
+            migration_version: migration_version,
+            table_options: table_options,
+          )
+        end
       end
     end
   end
