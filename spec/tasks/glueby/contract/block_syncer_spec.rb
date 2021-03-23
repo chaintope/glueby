cd192f711fa991c4751df200ac0f00b84b171de0
okashiwa <tkm05oshikawa12@gmail.com>
2021-03-23 14:37:06 +0900

    modify block_syncer test about subject.invoke

--- spec/tasks/glueby/contract/block_syncer_spec.rb
+++ spec/tasks/glueby/contract/block_syncer_spec.rb
@@ -13,18 +13,18 @@ RSpec.describe 'Glueby::Contract::Task::BlockSyncer', active_record: true do
   include_context 'Set rpc responses'
 
   describe '#start' do
-    subject { Rake.application['glueby:contract:block_syncer:start'].execute }
+    subject { Rake.application['glueby:contract:block_syncer:start'] }
 
     it do
       expect(rpc).to receive(:getblockcount).once
       expect(rpc).to receive(:getblock).twice
       expect(rpc).to receive(:getblockhash).twice
       expect(rpc).to receive(:getrawtransaction).exactly(4).times
-      expect { subject }.to change { Glueby::AR::SystemInformation.synced_block_height }.from(1).to(2)
-    end
-
-    it do 
-      expect { subject }.to change { Glueby::Internal::Wallet::AR::Utxo.count }.from(1).to(2)
+      expect(Glueby::AR::SystemInformation.synced_block_height).to eq(1)
+      expect(Glueby::Internal::Wallet::AR::Utxo.count).to eq(1)
+      subject.invoke
+      expect(Glueby::AR::SystemInformation.synced_block_height).to eq(2)
+      expect(Glueby::Internal::Wallet::AR::Utxo.count).to eq(2)
     end
   end
 
