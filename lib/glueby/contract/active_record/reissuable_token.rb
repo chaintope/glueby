fecfbb8b85e7acfc0b5202a131b59d4d7f7c6b97
wokashi <tkm05oshikawa12@gmail.com>
2021-05-19 18:51:39 +0900

    add comment

--- lib/glueby/contract/active_record/reissuable_token.rb
+++ lib/glueby/contract/active_record/reissuable_token.rb
@@ -3,10 +3,17 @@ module Glueby
     module AR
       class ReissuableToken < ::ActiveRecord::Base
 
+        # Get the script_pubkey corresponding to the color_id in Tapyrus::Script format
+        # @param [String] color_id
+        # @return [Tapyrus::Script]
         def self.script_pubkey(color_id)
-          Glueby::Contract::AR::ReissuableToken.where(color_id: color_id).pluck(:script_pubkey).first
+          script_pubkey_hex = Glueby::Contract::AR::ReissuableToken.where(color_id: color_id).pluck(:script_pubkey).first
+          Tapyrus::Script.parse_from_payload(script_pubkey_hex.htb)
         end
 
+        # Check if the color_id is already stored
+        # @param [String] color_id
+        # @return [Boolean]
         def self.saved?(color_id)
           Glueby::Contract::AR::ReissuableToken.where(color_id: color_id).exists?
         end
