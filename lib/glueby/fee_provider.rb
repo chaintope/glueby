f9b3d37210c50b9e8d41b3cdc677d0ae15503377
Kohei Taniguchi <kohei@chaintope.com>
2021-05-26 14:46:36 +0900

    Add a check for SIGHASH_TYPE hash ANYONECANPAY flag in transactions which are processed in FeeProvider

--- lib/glueby/fee_provider.rb
+++ lib/glueby/fee_provider.rb
@@ -36,11 +36,18 @@ module Glueby
       @utxo_pool_size = (FeeProvider.config && FeeProvidr.config[:utxo_pool_size]) || DEFAULT_UTXO_POOL_SIZE
     end
 
-
     # Provide an input for fee to the tx.
     # @param [Tapyrus::Tx] tx - The tx that is provided fee as a input. It should be signed with ANYONECANPAY flag.
     # @return [Tapyrus::Tx]
+    # @raise [ArgumentError] If the signatures that the tx inputs has don't have ANYONECANPAY flag.
     def provide(tx)
+      tx.inputs.each do |txin|
+        sig = get_signature(txin.script_sig)
+        unless sig[-1].unpack1('C') & Tapyrus::SIGHASH_TYPE[:anyonecanpay] == Tapyrus::SIGHASH_TYPE[:anyonecanpay]
+          raise ArgumentError, 'All the signatures that the tx inputs has should have ANYONECANPAY flag.'
+        end
+      end
+
       utxo = utxo_for_fee
       out_point = Tapyrus::OutPoint.new(utxo[:txid].rhex, utxo[:vout])
       tx.inputs << Tapyrus::TxIn.new(out_point: out_point)
@@ -54,6 +61,11 @@ module Glueby
       utxo = wallet.list_unspent.find { |o| !o[:color_id] && o[:amount] == fixed_fee }
       raise NoUtxosInUtxoPool, 'No UTXOs in Fee Provider UTXO pool. UTXOs should be created with "glueby:fee_provider:manage_utxo_pool" rake task' unless utxo
       utxo
+    end
+
+    # Get Signature from P2PKH or CP2PKH script sig
+    def get_signature(script_sig)
+      script_sig.chunks.first.pushed_data
     end
   end
 end
--- spec/glueby/fee_provider_spec.rb
+++ spec/glueby/fee_provider_spec.rb
@@ -40,5 +40,13 @@ RSpec.describe 'Glueby::FeeProvider' do
       )
       subject
     end
+
+    context 'Signatures in tx don\'t have ANYONECNAPAY flag' do
+      let(:tx) { Tapyrus::Tx.parse_from_payload("01000000018939a2974964f144941ed79c60a0046ee9f842f3b60453ea7d4533601c7ca030000000006a473044022004e5c255e7f397d3ea67e104b00bebfc31cff8d8f57b4c4972473936dda35d8e0220562676a1d1dfd85c89d596e7e96eaa178f8ef4b30c109c561612435137a8860401210280b5f253e612294a2c15a912bbb39b1d5ea48163c14bff56fd1a7079fed23258ffffffff02e8030000000000001976a91402b5fb1195e4c7b069ac33751d03d73a216babe588ac28230000000000001976a9148981ef639f525dc87b0c92ea9fb9af5148cf229688ac00000000".htb) }
+
+      it 'raise ArgumentError' do
+        expect { subject }.to raise_error(ArgumentError, 'All the signatures that the tx inputs has should have ANYONECANPAY flag.')
+      end
+    end
   end
 end
