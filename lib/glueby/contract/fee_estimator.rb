f97b742777f40102eb2c0971799988dbcb61136b
Kohei Taniguchi <kohei@chaintope.com>
2021-05-28 17:18:40 +0900

    Make fee estimators return 0 if fee_provider_bears! configuration is enabled

--- lib/glueby/contract/fee_estimator.rb
+++ lib/glueby/contract/fee_estimator.rb
@@ -1,8 +1,22 @@
 module Glueby
   module Contract
     module FeeEstimator
+      # @param [Tapyrus::Tx] tx - The target tx
       # @return fee by tapyrus(not TPC).
-      def fee(_tx); end
+      def fee(tx)
+        return 0 if Glueby.configuration.fee_provider_bears?
+        estimate_fee(tx)
+      end
+
+      private
+
+      # @private
+      # @abstract Override in subclasses. This is would be implemented an actual estimation logic.
+      # @param [Tapyrus::Tx] tx - The target tx
+      # @return fee by tapyrus(not TPC).
+      def estimate_fee(tx)
+        raise NotImplementedError
+      end
     end
 
     class FixedFeeEstimator
@@ -12,8 +26,11 @@ module Glueby
         @fixed_fee = fixed_fee
       end
 
+      private
+
+      # @private
       # @return fee by tapyrus(not TPC).
-      def fee(_tx)
+      def estimate_fee(_tx)
         @fixed_fee
       end
     end
