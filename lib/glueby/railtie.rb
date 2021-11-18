d2db1a9a11d5afa67face8b941b015c4ea207c18
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2021-11-19 02:46:49 +0900

    Load rake task for utxo provider

--- lib/glueby/railtie.rb
+++ lib/glueby/railtie.rb
@@ -5,6 +5,7 @@ module Glueby
       load "tasks/glueby/contract/timestamp.rake"
       load "tasks/glueby/block_syncer.rake"
       load "tasks/glueby/fee_provider.rake"
+      load "tasks/glueby/utxo_provider.rake"
     end
   end
 end
