7e46e4c3b98dca4f09091d94ebb6d217abf9cd0c
bonn1112 <akutsu0521@gmail.com>
2021-11-23 22:34:09 -0500

    fix payment functional test

--- spec/functional/payment_spec.rb
+++ spec/functional/payment_spec.rb
@@ -15,14 +15,27 @@ RSpec.describe 'Payment Contract', functional: true do
       let(:fee) { 10_000 }
       let(:fee_estimator) { Glueby::Contract::FixedFeeEstimator.new(fixed_fee: fee) }
 
-      before do
-        process_block(to_address: sender.internal_wallet.receive_address)
-        # create labeled utxo
-        process_block(to_address: receiver.internal_wallet.receive_address('labeled'))
-        before_balance
-      end
-
       it 'pays TPC to another wallet' do
+        # create labeled utxo
+        process_block(to_address: sender.internal_wallet.receive_address('labeled'))
+
+        expect do
+          Glueby::Contract::Payment.transfer(
+            sender: sender,
+            receiver_address: receiver.internal_wallet.receive_address,
+            amount: 10_000,
+            fee_estimator: fee_estimator
+          )
+        end.to raise_error(Glueby::Contract::Errors::InsufficientFunds)
+
+        # should not consume 'labeled' utxos
+        expect(sender.internal_wallet.list_unspent(false, 'labeled').size).to eq(1)
+        expect(sender.internal_wallet.list_unspent(false, 'labeled')[0][:amount]).to eq(5_000_000_000)
+
+        # create a usable utxo for payment
+        process_block(to_address: sender.internal_wallet.receive_address)
+        before_balance
+
         Glueby::Contract::Payment.transfer(
           sender: sender,
           receiver_address: receiver.internal_wallet.receive_address,
@@ -35,8 +48,8 @@ RSpec.describe 'Payment Contract', functional: true do
         # receiver got the sent amount
         expect(receiver.balances(false)['']).to eq(10_000)
         # should not consume 'labeled' utxos
-        expect(receiver.internal_wallet.list_unspent(false, 'labeled').size).to eq(1)
-        expect(receiver.internal_wallet.list_unspent(false, 'labeled')[0][:amount]).to eq(5_000_000_000)
+        expect(sender.internal_wallet.list_unspent(false, 'labeled').size).to eq(1)
+        expect(sender.internal_wallet.list_unspent(false, 'labeled')[0][:amount]).to eq(5_000_000_000)
       end
     end
 
@@ -46,28 +59,41 @@ RSpec.describe 'Payment Contract', functional: true do
       let!(:sender) { Glueby::Wallet.create }
       let!(:receiver) { Glueby::Wallet.create }
       let(:before_balance) { sender.balances(false)[''] }
-
-      before do
-        process_block(to_address: sender.internal_wallet.receive_address)
-        # create labeled utxo
-        process_block(to_address: receiver.internal_wallet.receive_address('labeled'))
-        before_balance
-      end
+      let(:fixed_fee) { 1_000 }
 
       it 'pays TPC to another wallet' do
+        # create labeled utxo
+        process_block(to_address: sender.internal_wallet.receive_address('labeled'))
+
+        expect do
+          Glueby::Contract::Payment.transfer(
+            sender: sender,
+            receiver_address: receiver.internal_wallet.receive_address,
+            amount: 10_000
+          )
+        end.to raise_error(Glueby::Contract::Errors::InsufficientFunds)
+
+        # should not consume 'labeled' utxos
+        expect(sender.internal_wallet.list_unspent(false, 'labeled').size).to eq(1)
+        expect(sender.internal_wallet.list_unspent(false, 'labeled')[0][:amount]).to eq(5_000_000_000 + fixed_fee)
+
+        # create a usable utxo for payment
+        process_block(to_address: sender.internal_wallet.receive_address)
+        before_balance
+
         tx = Glueby::Contract::Payment.transfer(
           sender: sender,
           receiver_address: receiver.internal_wallet.receive_address,
           amount: 10_000
         )
 
-        # sender lose just sent amount.
+        # sender lose sent amount
         expect(sender.balances(false)['']).to eq(before_balance - 10_000)
         # receiver got the sent amount
         expect(receiver.balances(false)['']).to eq(10_000)
         # should not consume 'labeled' utxos
-        expect(receiver.internal_wallet.list_unspent(false, 'labeled').size).to eq(1)
-        expect(receiver.internal_wallet.list_unspent(false, 'labeled')[0][:amount]).to eq(5_000_000_000)
+        expect(sender.internal_wallet.list_unspent(false, 'labeled').size).to eq(1)
+        expect(sender.internal_wallet.list_unspent(false, 'labeled')[0][:amount]).to eq(5_000_000_000 + fixed_fee)
 
         sighashtype = tx.inputs[-1].script_sig.chunks.first.pushed_data[-1].unpack1('C')
         expect(sighashtype).to eq(Tapyrus::SIGHASH_TYPE[:all])
