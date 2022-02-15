2a7621ef980ece7d74bc4705dcef567f91609c65
oshikawatkm <tkm05oshikawa12@gmail.com>
2022-02-15 14:33:55 +0900

    add interfaces on system_information

--- lib/glueby/active_record/system_information.rb
+++ lib/glueby/active_record/system_information.rb
@@ -12,10 +12,36 @@ module Glueby
         find_by(info_key: "use_only_finalized_utxo")&.int_value != 0
       end
 
+      # 
+      def self.set_use_only_finalized_utxo(status)
+        if column_names.include? "use_only_finalized_utxo"
+          current = find_by(info_key: "use_only_finalized_utxo")
+          current.update!(info_value: status)
+        else
+          create!(
+            info_key: "use_only_finalized_utxo", 
+            info_value: status
+          )
+        end
+      end
+
       # Return default value of the utxo provider
       # @return [Integer] default value of utxo provider
       def self.utxo_provider_default_value
         find_by(info_key: "utxo_provider_default_value")&.int_value
+      end
+
+      # 
+      def self.set_utxo_provider_default_value(value)
+        if column_names.include? "utxo_provider_default_value"
+          current = find_by(info_key: "utxo_provider_default_value")
+          current.update!(info_value: value)
+        else
+          create!(
+            info_key: "utxo_provider_default_value", 
+            info_value: value
+          )
+        end
       end
 
       # Return pool size of the utxo provider
@@ -24,9 +50,36 @@ module Glueby
         find_by(info_key: "utxo_provider_pool_size")&.int_value
       end
 
+      # 
       def self.set_utxo_provider_pool_size(size)
-        utxo = find_by(info_key: "utxo_provider_pool_size")
-        utxo.update!(info_value: size)
+        if column_names.include? "utxo_provider_pool_size"
+          current = find_by(info_key: "utxo_provider_pool_size")
+          current.update!(info_value: size)
+        else
+          create!(
+            info_key: "utxo_provider_pool_size", 
+            info_value: size
+          )
+        end
+      end
+
+      # 
+      # @return [Boolean] true 
+      def self.broadcast_on_background?
+        find_by(info_key: "broadcast_on_background")&.int_value != 0
+      end
+
+      # 
+      def self.set_broadcast_on_background(status)
+        if column_names.include? "broadcast_on_background"
+          current = find_by(info_key: "broadcast_on_background")
+          current.update!(info_value: status)
+        else
+          create!(
+            info_key: "broadcast_on_background", 
+            info_value: status
+          )
+        end
       end
 
       def int_value
--- spec/glueby/active_record/system_informations_spec.rb
+++ spec/glueby/active_record/system_informations_spec.rb
@@ -54,25 +54,134 @@ RSpec.describe 'Glueby::AR::SystemInformation', active_record: true do
     end
   end
 
+  describe '.set_use_only_finalized_utxo' do
+    subject { Glueby::AR::SystemInformation.set_use_only_finalized_utxo(status) }
+
+    context do 
+      let(:status) { true }
+
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.use_only_finalized_utxo?).to be_truthy
+      end
+    end
+
+    context do 
+      let(:status) { false }
+
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.use_only_finalized_utxo?).to be_falsy
+      end
+    end
+  end
+
   describe '.int_value' do
     subject { Glueby::AR::SystemInformation.synced_block_height }
 
     it { expect(subject.int_value).to be 0 }
   end
 
+  describe '.set_utxo_provider_default_value' do
+    let(:value) { 5 }
+    subject { Glueby::AR::SystemInformation.set_utxo_provider_default_value(value) }
+
+    context 'If utxo_provider_default_value column is present' do
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.utxo_provider_default_value).to eq value
+      end
+    end
+
+    context 'If utxo_provider_default_value column is not present' do      
+      before do
+        Glueby::AR::SystemInformation.create(
+          info_key: "utxo_provider_default_value",
+          info_value: "0"
+        )
+      end 
+
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.utxo_provider_default_value).to eq value 
+      end
+    end
+  end
+
   describe '.set_utxo_provider_pool_size' do
-    before do
-      Glueby::AR::SystemInformation.create(
-        info_key: "utxo_provider_pool_size",
-        info_value: "0"
-      )
-    end 
     let(:size) { 5 }
     subject { Glueby::AR::SystemInformation.set_utxo_provider_pool_size(size) }
 
-    it do
-      subject
-      expect(Glueby::AR::SystemInformation.utxo_provider_pool_size).to eq size 
+    context 'If utxo_provider_pool_size column is present' do
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.utxo_provider_pool_size).to eq size
+      end
+    end
+
+    context 'If utxo_provider_pool_size column is not present' do      
+      before do
+        Glueby::AR::SystemInformation.create(
+          info_key: "utxo_provider_pool_size",
+          info_value: "0"
+        )
+      end 
+
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.utxo_provider_pool_size).to eq size 
+      end
+    end
+  end
+
+  describe '.broadcast_on_background?' do
+    subject { Glueby::AR::SystemInformation.broadcast_on_background? }
+
+    context 'default value' do
+      it { expect(subject).to be_truthy }
+    end
+
+    context 'if info_value is 0' do
+      before do
+        Glueby::AR::SystemInformation.create(
+          info_key: 'broadcast_on_background',
+          info_value: '0'
+        )
+      end
+      it { expect(subject).to be_falsy }
+    end
+
+    context 'if info_value is 1' do
+      before do
+        Glueby::AR::SystemInformation.create(
+          info_key: 'broadcast_on_background',
+          info_value: '1'
+        )
+      end
+
+      it { expect(subject).to be_truthy }
+    end
+  end
+
+  describe '.set_broadcast_on_background' do
+    subject { Glueby::AR::SystemInformation.set_broadcast_on_background(status) }
+
+    context do 
+      let(:status) { true }
+
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.broadcast_on_background?).to be_truthy
+      end
+    end
+
+    context do 
+      let(:status) { false }
+
+      it do
+        subject
+        expect(Glueby::AR::SystemInformation.broadcast_on_background?).to be_falsy
+      end
     end
   end
 
