d20cc3be3aba3023a6b4c2a49d4ff6aef8441877
wokashi <55338564+oshikawatkm@users.noreply.github.com>
2022-02-17 13:59:33 +0900

    add system information interfaces (#124)
    * add interfaces on system_information
    
    * add comment
    
    * modify test
    
    * comment out test
    
    * use boolen as arguments
    
    * add convert_to_int method
    
    * modify method name
    
    Co-authored-by: --local <--local>

--- lib/glueby/active_record/system_information.rb
+++ lib/glueby/active_record/system_information.rb
@@ -12,10 +12,39 @@ module Glueby
         find_by(info_key: "use_only_finalized_utxo")&.int_value != 0
       end
 
+
+      # Set use_only_finalized_utxo
+      # @param [Boolean] status status of whether to use only finalized utxo
+      def self.set_use_only_finalized_utxo(status)
+        current = find_by(info_key: "use_only_finalized_utxo")
+        if current
+          current.update!(info_value: boolean_to_string(status))
+        else
+          create!(
+            info_key: "use_only_finalized_utxo", 
+            info_value: boolean_to_string(status)
+          )
+        end
+      end
+
       # Return default value of the utxo provider
       # @return [Integer] default value of utxo provider
       def self.utxo_provider_default_value
         find_by(info_key: "utxo_provider_default_value")&.int_value
+      end
+
+      # Set utxo_provider_default_value
+      # @param [Integer] value default value for utxo provider
+      def self.set_utxo_provider_default_value(value)
+        current = find_by(info_key: "utxo_provider_default_value")
+        if current
+          current.update!(info_value: value)
+        else
+          create!(
+            info_key: "utxo_provider_default_value", 
+            info_value: value
+          )
+        end
       end
 
       # Return pool size of the utxo provider
@@ -24,13 +53,48 @@ module Glueby
         find_by(info_key: "utxo_provider_pool_size")&.int_value
       end
 
+      # Set utxo_provider_pool_size
+      # @param [Integer] size pool size of the utxo provider
       def self.set_utxo_provider_pool_size(size)
-        utxo = find_by(info_key: "utxo_provider_pool_size")
-        utxo.update!(info_value: size)
+        current = find_by(info_key: "utxo_provider_pool_size")
+        if current
+          current.update!(info_value: size)
+        else
+          create!(
+            info_key: "utxo_provider_pool_size", 
+            info_value: size
+          )
+        end
+      end
+
+      # If return timestamp is to be executed immediately
+      # @return [Boolean] true status of broadcast_on_background
+      def self.broadcast_on_background?
+        find_by(info_key: "broadcast_on_background")&.int_value != 0
+      end
+
+      # Set the status of broadcast_on_background
+      # @param [Boolean] status status of broadcast_on_background
+      def self.set_broadcast_on_background(status)
+        current = find_by(info_key: "broadcast_on_background")
+        if current
+         current.update!(info_value: boolean_to_string(status))
+        else
+          create!(
+            info_key: "broadcast_on_background", 
+            info_value: boolean_to_string(status)
+          )
+        end
       end
 
       def int_value
         info_value.to_i
+      end
+
+      private
+
+      def self.boolean_to_string(status)
+        status ? "1" : "0"
       end
 
     end
