78d3b8e97135d1e9c187ef7340d659293f7e862b
Kohei Taniguchi <kohei@chaintope.com>
2022-02-26 01:16:40 +0900

    Add comment for Glueby::Contract::AR::Timestamp#utxo

--- lib/glueby/contract/timestamp/tx_builder.rb
+++ lib/glueby/contract/timestamp/tx_builder.rb
@@ -1,7 +1,7 @@
 module Glueby
   module Contract
     class Timestamp
-      class TxBuilder
+      module TxBuilder
         autoload :SimpleTxBuilder, 'glueby/contract/timestamp/tx_builder/simeple_tx_builder'
         autoload :TrackableTxBuilder, 'glueby/contract/timestamp/tx_builder/trackable_tx_builder'
         autoload :UpdatingTrackableTxBuilder, 'glueby/contract/timestamp/tx_builder/updating_trackable_tx_builder'
