045881828eb554e05d559868bd70c0a839b3474e
Kohei Taniguchi <kohei@chaintope.com>
2022-03-08 16:53:34 +0900

    Add a fee estimator that calculate fees from tx

---
+++ lib/glueby/contract/fee_estimator/calc.rb
@@ -0,0 +1,34 @@
+module Glueby
+  module Contract
+    module FeeEstimator
+      # It calculates actual minimum fee to broadcast txs.
+      class Calc
+        include FeeEstimator
+
+        # This is same with Tapyrus Core's default min_relay_fee value.(tapyrus/kB)
+        DEFAULT_FEE_RATE = 1_000
+
+        # @!attribute [r] fee_rate
+        #  @return [Integer] the fee rate(tapyrus/kB) that is used actual fee calculation
+        attr_reader :fee_rate
+
+        class << self
+          # @!attribute [rw] default_fee_rate
+          #  @return [Integer] The global fee rate configuration. All instances use this value as the default.
+          attr_accessor :default_fee_rate
+        end
+
+        # @param [Integer] fee_rate
+        def initialize(fee_rate: Calc.default_fee_rate || DEFAULT_FEE_RATE)
+          @fee_rate = fee_rate
+        end
+
+        private
+
+        def estimate_fee(tx)
+          ((tx.to_payload.bytesize / 1000.0) * fee_rate).ceil
+        end
+      end
+    end
+  end
+end
---
+++ spec/glueby/contract/fee_estimator/calc_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe 'Glueby::Contract::FeeEstimator::Calc' do
+  describe '#fee' do
+    subject { Glueby::Contract::FeeEstimator::Calc.new.fee(tx) }
+
+    let(:tx) { double('tx', to_payload: "\x00" * bytesize) }
+    let(:bytesize) { 219 }
+
+    it { is_expected.to eq 219 }
+    it { is_expected.to be_a(Integer) }
+
+    context 'if specify fee_rate' do
+      subject { Glueby::Contract::FeeEstimator::Calc.new(fee_rate: 100).fee(tx) }
+
+      it { is_expected.to eq 22 }
+    end
+
+    context 'default_fee_rate is specified' do
+      before do
+        Glueby::Contract::FeeEstimator::Calc.default_fee_rate = 600
+      end
+
+      after do
+        Glueby::Contract::FixedFeeEstimator.default_fixed_fee = nil
+      end
+
+      it { is_expected.to eq 132 }
+    end
+  end
+end
