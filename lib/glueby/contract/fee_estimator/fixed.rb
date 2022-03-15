7bdbf7decbf568e477de268a34b27e7b82b37d1a
Kohei Taniguchi <kohei@chaintope.com>
2022-03-15 15:40:10 +0900

    Constantize default fee in fixed fee estimator

--- lib/glueby/contract/fee_estimator/fixed.rb
+++ lib/glueby/contract/fee_estimator/fixed.rb
@@ -4,13 +4,16 @@ module Glueby
       class Fixed
         include FeeEstimator
 
+        # Default fee in tapyrus that is used if it is not configured
+        DEFAULT_FEE = 10_000
+
         attr_reader :fixed_fee
 
         class << self
           attr_accessor :default_fixed_fee
         end
 
-        def initialize(fixed_fee: Fixed.default_fixed_fee || 10_000)
+        def initialize(fixed_fee: Fixed.default_fixed_fee || DEFAULT_FEE)
           @fixed_fee = fixed_fee
         end
 
