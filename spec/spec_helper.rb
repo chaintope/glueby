69b0aa837e08a8a8582cf4cefc051f062ecd2127
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2022-04-22 17:55:51 +0900

    Use v0.5.1 image for Github Actions build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'docker'
 require 'active_record'
 
 TAPYRUSD_CONTAINER_NAME = 'glueby-tapyrusd'
+TAPYRUSD_VERSION = 'v0.5.1'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -144,10 +145,10 @@ class TapyrusCoreContainer
   attr_reader :container
 
   def setup
-    Docker::Image.get('tapyrus/tapyrusd:edge')
+    Docker::Image.get("tapyrus/tapyrusd:#{TAPYRUSD_VERSION}")
     @container = Docker::Container.create({
       name: TAPYRUSD_CONTAINER_NAME,
-      "Image"=>"tapyrus/tapyrusd:edge",
+      "Image"=>"tapyrus/tapyrusd:#{TAPYRUSD_VERSION}",
       "Env"=>[
         "GENESIS_BLOCK_WITH_SIG=0100000000000000000000000000000000000000000000000000000000000000000000002b5331139c6bc8646bb4e5737c51378133f70b9712b75548cb3c05f9188670e7440d295e7300c5640730c4634402a3e66fb5d921f76b48d8972a484cc0361e66ef74f45e012103af80b90d25145da28c583359beb47b21796b2fe1a23c1511e443e7a64dfdb27d40e05f064662d6b9acf65ae416379d82e11a9b78cdeb3a316d1057cd2780e3727f70a61f901d10acbe349cd11e04aa6b4351e782c44670aefbe138e99a5ce75ace01010000000100000000000000000000000000000000000000000000000000000000000000000000000000ffffffff0100f2052a010000001976a91445d405b9ed450fec89044f9b7a99a4ef6fe2cd3f88ac00000000"
       ],
