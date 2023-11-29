d01e3fc942c055ee42c5115db2817616baa88c4c
Yamaguchi <h_yamaguchi@chaintope.com>
2023-11-29 16:12:44 +0900

    Remove method Glueby::Internal::Wallet::AR::Wallet#token_utxos

--- lib/glueby/internal/wallet/active_record/wallet.rb
+++ lib/glueby/internal/wallet/active_record/wallet.rb
@@ -31,10 +31,6 @@ module Glueby
             Glueby::Internal::Wallet::AR::Utxo.where(key: keys)
           end
 
-          def token_utxos(color_id = Tapyrus::Color::ColorIdentifier.default)
-            utxos.where(color_id: color_id&.to_hex)
-          end
-
           private
 
           def sign_tx_for_p2pkh(tx, index, key, script_pubkey, sighashtype)
